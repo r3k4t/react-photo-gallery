e5a1ae2705bf0f0462e602e4873a6ad516436f60
Rahat Khan Tusar <gb229855@gmail.com>
2021-10-18 16:45:03 +0600

    react photo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+      <h1 >Photo Gallery Website</h1>
+
+
+      <div>
+
+        <img src="images/img1.jpg" class="image-size"></img>
+        <img src="images/img2.jpg" class="image-size"></img>
+        <img src="images/img3.jpg" class="image-size"></img>
+        <img src="images/img4.jpg" class="image-size"></img>
+        <img src="images/img5.jpg" class="image-size"></img>
+        <img src="images/img6.jpg" class="image-size"></img>
+        <img src="images/img7.jpg" class="image-size"></img>
+        <img src="images/img8.jpg" class="image-size"></img>
+        <img src="images/img9.jpg" class="image-size"></img>
+        <img src="images/img10.jpg" class="image-size"></img>
+        <img src="images/img11.jpg" class="image-size"></img>
+        <img src="images/img12.jpg" class="image-size"></img>
+        <img src="images/img13.jpg" class="image-size"></img>
+        <img src="images/img14.jpg" class="image-size"></img>
+        <img src="images/img15.jpg" class="image-size"></img>
+        <img src="images/img16.jpg" class="image-size"></img>
+        <img src="images/img17.jpg" class="image-size"></img>
+        <img src="images/img18.jpg" class="image-size"></img>
+        <img src="images/img19.jpg" class="image-size"></img>
+        <img src="images/img20.jpg" class="image-size"></img>
+
+
+
+
+
+
+
+      </div>
+    </div >
   );
 }
 
